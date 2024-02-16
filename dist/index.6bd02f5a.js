a070b1f79e15fa791b4088b6d31d674db84486b4
vitthal lambrut <vitthal.lambrut@firstcry.in>
2024-02-17 01:27:12 +0530

    changes in .gitignore

--- dist/index.6bd02f5a.js
+++
@@ -1,5 +0,0 @@
-const heading = React.createElement("h1", {}, "Hello world..! from React");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
-//# sourceMappingURL=index.6bd02f5a.js.map
